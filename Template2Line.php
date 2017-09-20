fd2bd4784c0b96f4f86b29c9da899434de691263
MacShare <cmod.maod@gmail.com>
2017-09-20 09:17:14 +0700

    Update Template2Line.php

--- Template2Line.php
+++ Template2Line.php
@@ -336,7 +336,8 @@ if (!is_null($text)) {
 	$url = 'https://api.line.me/v2/bot/message/push';
 	$data = [
   		//'to' => 'Cc7ac9ccc51f05b2a60a1abed8cf85723',
-		'to' => 'Cd90b89c39f5a695f6d6996c80829e269',
+		//'to' => 'Cd90b89c39f5a695f6d6996c80829e269',
+		'to' => 'U554a18dbd36996fdb3dd95c218cf6db0',
 		'messages' => [$messages]
 		];
 	$post = json_encode($data);
