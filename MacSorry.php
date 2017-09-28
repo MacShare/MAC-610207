c448cfa21c0d9092ff9deaab2cc91770cf198b02
MacShare <cmod.maod@gmail.com>
2017-09-28 21:44:04 +0700

    Create MacSorry.php

---
+++ MacSorry.php
@@ -0,0 +1,17 @@
+<?php
+$SorryTxt = "ขออภัยคุณเข้าสู่ระบบไม่ถูกต้อง";
+$url = 'https://api.line.me/v2/bot/message/reply';
+$postbackdata = tt1($SorryTxt);
+$data = data1($replyToken,$postbackdata);
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";	
+?>
