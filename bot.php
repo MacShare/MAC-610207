21637e9e48b39dbd4f82f1cdefb08f1f4c368836
MacShare <cmod.maod@gmail.com>
2017-09-28 22:08:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -156,29 +156,10 @@ if (!is_null($events['events'])) {
 			//$postbackdata = $postbackdata;
 			//$postbackdata = "Test Postback";
 			if (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0')) {
-				/*if ($postbackdata == 'action=SwitchingDiagram') {
-					$originalContentUrl = $ecsURL."Mac_Power.jpg";
-					$previewImageUrl = $ecsURL."SW_BK.JPG";
-					$url = 'https://api.line.me/v2/bot/message/reply';
-					$postbackdata = im1($originalContentUrl,$previewImageUrl);
-					$data = data1($replyToken,$postbackdata);
-					$post = json_encode($data);
-					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-					$ch = curl_init($url);
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-					$result = curl_exec($ch);
-					curl_close($ch);
-					echo $result . "\r\n";	
-				}*/
 				switch ($postbackdata) {
 					case "action=Powerflow":
 						// Power Flow Last Time
-						//include 'MacPowerFlow.php';
-						include 'MacDiagram.php';
+						include 'MacPowerFlow.php';
 						break;					
 					case "action=SwitchingDiagram":
 						// Switching Diagram
@@ -186,8 +167,7 @@ if (!is_null($events['events'])) {
 						break;
 					case "action=TransformerLoading":
 						// Transformer Last Day
-						//include 'MacTransformer.php';
-						include 'MacSorry.php';
+						include 'MacTransformer.php';
 						break;						
 					default:
 						include 'MacSorry.php';						
