768adf906bc5488682a16e13446e5694f505dce6
MacShare <cmod.maod@gmail.com>
2017-10-13 21:24:26 +0700

    Create MacDG_LLA_LPR.php

---
+++ MacDG_LLA_LPR.php
@@ -0,0 +1,25 @@
+<?php
+$Org1 = $ecsURL."BK_1.JPG";
+$Pv1 = $ecsURL."SW_BK.JPG";
+
+$Org2 = $ecsURL."BN_1.JPG";
+$Pv2 = $ecsURL."SW_BN.JPG";
+
+$Org3 = $ecsURL."BPL_1.JPG";
+$Pv3 = $ecsURL."SW_BPL.JPG";
+
+
+$url = 'https://api.line.me/v2/bot/message/reply';
+$data = im3($replyToken,$Org1,$Pv1,$Org2,$Pv2,$Org3,$Pv3);
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";	
+?>
