5bc2adc901e7d067dfdca257add5e4305474726c
MacShare <cmod.maod@gmail.com>
2017-12-07 19:56:39 +0700

    Create MacTLLoad_Last_Day.php

---
+++ MacTLLoad_Last_Day.php
@@ -0,0 +1,19 @@
+<?php
+$Org1 = $ecsURL."TL_Load.jpg";
+$Pv1 = $ecsURL."TL_Load.jpg";
+
+$url = 'https://api.line.me/v2/bot/message/reply';
+$messages = im1($Org1,$Pv1);
+$data = data1($replyToken,$messages);
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";	
+?>
