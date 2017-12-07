7c3538877d1894265ac1ae68058f4ad4ce088ea0
MacShare <cmod.maod@gmail.com>
2017-12-07 19:21:13 +0700

    Update MacTransformer.php

--- MacTransformer.php
+++ MacTransformer.php
@@ -7,12 +7,12 @@
 		];
 	$col1_2 = [
 		'type' => 'postback',
-		'label' => 'Transfermer Load Last Day',
+		'label' => 'Tx Load Last Day',
 		'data' => 'action=TxLoad2_Last_Day'	
 		];
 	$col1_3 = [
 		'type' => 'postback',
-		'label' => 'Transmission Line Last Day',
+		'label' => 'TL Load Last Day',
 		'data' => 'action=TLLoad_Last_Day'	
 		];
 	$col1 = [
