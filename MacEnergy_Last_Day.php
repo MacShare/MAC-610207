7fd74baa564bbfaf455f50703fc4acdc4f775717
MacShare <cmod.maod@gmail.com>
2017-12-07 19:40:28 +0700

    Update MacEnergy_Last_Day.php

--- MacEnergy_Last_Day.php
+++ MacEnergy_Last_Day.php
@@ -4,7 +4,7 @@ $Pv1 = $ecsURL."Energy.jpg";
 
 $url = 'https://api.line.me/v2/bot/message/reply';
 $messages = im1($Org1,$Pv1);
-$data = data1($replyToken,$messages)
+$data = data1($replyToken,$messages);
 $post = json_encode($data);
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
