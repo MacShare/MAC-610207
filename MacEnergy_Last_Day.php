29de67c8b9efd34c7871175cd8aef20e774bad97
MacShare <cmod.maod@gmail.com>
2017-12-07 19:31:53 +0700

    Create MacEnergy_Last_Day.php

---
+++ MacEnergy_Last_Day.php
@@ -0,0 +1,18 @@
+<?php
+$Org1 = $ecsURL."Energy.jpg";
+$Pv1 = $ecsURL."Energy.jpg";
+
+$url = 'https://api.line.me/v2/bot/message/reply';
+$data = im1($replyToken,$Org1,$Pv1);
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";	
+?>
