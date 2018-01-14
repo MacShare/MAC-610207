931f1f45783edc1388e27e9585d48c320dded532
MacShare <cmod.maod@gmail.com>
2018-01-14 08:07:40 +0700

    Update MAC2NB_WorkAround.php

--- MAC2NB_WorkAround.php
+++ MAC2NB_WorkAround.php
@@ -23,7 +23,7 @@ if (!is_null($text)) {
 	$messages = t1($text);
 	$url = 'https://api.line.me/v2/bot/message/push';
 	$data = [
-  		'to' => 'Cd90b89c39f5a695f6d6996c80829e269',
+  		'to' => 'C6e6e7cf07e32bc9700dd4d94df26a851',
 		'messages' => [$messages]
 		];
 	$post = json_encode($data);
