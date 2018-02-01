736d38f8adf1252647a8d6a186bbda19ebb97f53
MacShare <cmod.maod@gmail.com>
2018-02-01 20:46:28 +0700

    Update MacDG_BK.php

--- MacDG_BK.php
+++ MacDG_BK.php
@@ -1,15 +1,10 @@
 <?php
 $Org1 = $ecsURL."BK_1.JPG";
 $Pv1 = $ecsURL."SW_BK.JPG";
-$Org2 = $ecsURL."BN_1.JPG";
-$Pv2 = $ecsURL."SW_BN.JPG";
-$Org3 = $ecsURL."BPL_1.JPG";
-$Pv3 = $ecsURL."SW_BPL.JPG";
-$Org4 = $ecsURL."CHW_1.JPG";
-$Pv4 = $ecsURL."SW_CHW.JPG";
+
 
 $url = 'https://api.line.me/v2/bot/message/reply';
-$data = im4($replyToken,$Org1,$Pv1,$Org2,$Pv2,$Org3,$Pv3,$Org4,$Pv4);
+$data = im1($replyToken,$Org1,$Pv1);
 $post = json_encode($data);
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
