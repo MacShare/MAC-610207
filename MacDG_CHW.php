2ee0fdaac217ca522246a9a34a7b4c5d530f2640
MacShare <cmod.maod@gmail.com>
2018-02-01 21:06:34 +0700

    Create MacDG_CHW.php

---
+++ MacDG_CHW.php
@@ -0,0 +1,18 @@
+<?php
+$Org1 = $ecsURL."CHW_1.JPG";
+$Pv1 = $ecsURL."SW_CHW.JPG";
+$url = 'https://api.line.me/v2/bot/message/reply';
+$postbackdata = im1($Org1,$Pv1);
+$data = data1($replyToken,$postbackdata);
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";	
+?>
