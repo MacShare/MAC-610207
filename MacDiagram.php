fae564b5bb60ae7fde17e7b56c13506ae349813f
MacShare <cmod.maod@gmail.com>
2018-02-01 20:30:06 +0700

    Update MacDiagram.php

--- MacDiagram.php
+++ MacDiagram.php
@@ -2,17 +2,17 @@
 	//Column 1
 	$col1_1 = [
 		'type' => 'postback',
-		'label' => 'BK, BN, BPL, CHW',
+		'label' => 'BK',
 		'data' => 'action=BK_BN_BPL_CHW'	
 		];
 	$col1_2 = [
 		'type' => 'postback',
-		'label' => 'LLA, LPR',
+		'label' => 'BN',
 		'data' => 'action=LLA_LPR'	
 		];
 	$col1_3 = [
 		'type' => 'postback',
-		'label' => 'NB, NCO',
+		'label' => 'BPL',
 		'data' => 'action=NB_NCO'	
 		];
 	$col1 = [
@@ -25,17 +25,17 @@
 	//Column 2
 	$col2_1 = [
 		'type' => 'postback',
-		'label' => 'NV, ON',
+		'label' => 'CHW',
 		'data' => 'action=NV_ON'	
 		];
 	$col2_2 = [
 		'type' => 'postback',
-		'label' => 'RPS, RS, SB',
+		'label' => 'LLA',
 		'data' => 'action=RPS_RS_SB'	
 		];
 	$col2_3 = [
 		'type' => 'postback',
-		'label' => 'SNO, STB, TPR',
+		'label' => 'LPR',
 		'data' => 'action=SNO_STB_TPR'	
 		];
 	$col2 = [
@@ -48,17 +48,17 @@
 	//Column 3
 	$col3_1 = [
 		'type' => 'postback',
-		'label' => 'NV, ON',
+		'label' => 'NB',
 		'data' => 'action=NV_ON'	
 		];
 	$col3_2 = [
 		'type' => 'postback',
-		'label' => 'RPS, RS, SB',
+		'label' => 'NCO',
 		'data' => 'action=RPS_RS_SB'	
 		];
 	$col3_3 = [
 		'type' => 'postback',
-		'label' => 'SNO, STB, TPR',
+		'label' => 'NV',
 		'data' => 'action=SNO_STB_TPR'	
 		];
 	$col3 = [
@@ -68,14 +68,81 @@
 		'actions' => [$col3_1,$col3_2,$col3_3]
 		];
 
+	//Column 4
+	$col4_1 = [
+		'type' => 'postback',
+		'label' => 'ON',
+		'data' => 'action=NV_ON'	
+		];
+	$col4_2 = [
+		'type' => 'postback',
+		'label' => 'RPS',
+		'data' => 'action=RPS_RS_SB'	
+		];
+	$col4_3 = [
+		'type' => 'postback',
+		'label' => 'RS',
+		'data' => 'action=SNO_STB_TPR'	
+		];
+	$col4 = [
+		'thumbnailImageUrl' => $ecsURL.'ColMenu_2.JPG',
+		'title' => 'MAC',
+		'text' => 'Switching Digram',
+		'actions' => [$col4_1,$col4_2,$col4_3]
+		];
 
+	//Column 5
+	$col5_1 = [
+		'type' => 'postback',
+		'label' => 'SB',
+		'data' => 'action=NV_ON'	
+		];
+	$col5_2 = [
+		'type' => 'postback',
+		'label' => 'SNO',
+		'data' => 'action=RPS_RS_SB'	
+		];
+	$col5_3 = [
+		'type' => 'postback',
+		'label' => 'STB',
+		'data' => 'action=SNO_STB_TPR'	
+		];
+	$col5 = [
+		'thumbnailImageUrl' => $ecsURL.'ColMenu_2.JPG',
+		'title' => 'MAC',
+		'text' => 'Switching Digram',
+		'actions' => [$col5_1,$col5_2,$col5_3]
+		];
+
+	//Column 6
+	$col6_1 = [
+		'type' => 'postback',
+		'label' => 'TPR',
+		'data' => 'action=NV_ON'	
+		];
+	$col6_2 = [
+		'type' => 'postback',
+		'label' => 'Future',
+		'data' => 'action=RPS_RS_SB'	
+		];
+	$col6_3 = [
+		'type' => 'postback',
+		'label' => 'Future',
+		'data' => 'action=SNO_STB_TPR'	
+		];
+	$col6 = [
+		'thumbnailImageUrl' => $ecsURL.'ColMenu_2.JPG',
+		'title' => 'MAC',
+		'text' => 'Switching Digram',
+		'actions' => [$col6_1,$col6_2,$col6_3]
+		];
 
 	$messages = [
 		'type' => 'template',
 		'altText' => 'MAC Substation',
 		'template' => [
 			'type' => 'carousel',
-			'columns' => [$col1,$col2,$col2,$col2,$col2,$col2]
+			'columns' => [$col1,$col2,$col3,$col4,$col5,$col6]
 			]
 		];
 
