bc9147b80021568b15fdec3aeb49884428caa442
MacShare <cmod.maod@gmail.com>
2018-02-10 19:36:46 +0700

    Update MacLicense.php

--- MacLicense.php
+++ MacLicense.php
@@ -1,6 +1,21 @@
 <?php
 //Have $touserid, $toroomid, $togroupid From Bot
+$touserid = "U554a18dbd36996fdb3dd95c218cf6db0"
 $License = file_get_contents("License.txt");
+function iscontain($string,$find) {
+  $check = strpos($string, $find); 
+  if ($check === false) { 
+    return 0; }
+  else { 
+    return 1; 
+  } 
+} 
+if(iscontain($License,$touserid)) {
+  echo "true"; 
+}
+else {
+  echo "flase";
+}
 //Have $tTel From Bot
 $TelCheck = file_get_contents("FlashNews.txt");
 //$SorryTxt = strlen(substr($TelCheck,0,999));
