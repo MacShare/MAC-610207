d7d2f872ab5993c3d0bf59c8260869f94de7ea51
MacShare <cmod.maod@gmail.com>
2018-02-10 20:06:08 +0700

    Update MacAccept.php

--- MacAccept.php
+++ MacAccept.php
@@ -1,5 +1,19 @@
 <?php
 //Have $touserid, $toroomid, $togroupid, $userlicense From Bot
-$License = file_get_contents("License.txt");
-
+//Start $userlicense = "false" $License = file_get_contents("License.txt");
+$SorryTxt = "ขออภัยคุณยังไม่ได้ลงทะเบียน โปรดติดต่อ MAC ถ้าไม่ทราบวิธีลงทะเบียน";
+$url = 'https://api.line.me/v2/bot/message/reply';
+$postbackdata = t1($SorryTxt);
+$data = data1($replyToken,$postbackdata);
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";	
 ?>
