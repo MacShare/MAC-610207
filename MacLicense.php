d383936fc16a7909fedecfaaf8566949df51f808
MacShare <cmod.maod@gmail.com>
2018-02-10 19:45:19 +0700

    Update MacLicense.php

--- MacLicense.php
+++ MacLicense.php
@@ -1,6 +1,5 @@
 <?php
 //Have $touserid, $toroomid, $togroupid From Bot
-$touserid = "U554a18dbd36996fdb3dd95c218cf6db0";
 $License = file_get_contents("License.txt");
 function iscontain($string,$find) {
   $check = strpos($string, $find); 
@@ -16,9 +15,4 @@ if(iscontain($License,$touserid)) {
 else {
   $userlicense =  "flase";
 }
-//Have $tTel From Bot
-$TelCheck = file_get_contents("FlashNews.txt");
-//$SorryTxt = strlen(substr($TelCheck,0,999));
-$SorryTxt = substr($TelCheck,0,999);
-
 ?>
