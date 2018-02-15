2c30ca72aa92b56f1121d6aa56160f2144a35526
MacShare <cmod.maod@gmail.com>
2018-02-15 09:56:57 +0700

    Update receive_get.php

--- receive_get.php
+++ receive_get.php
@@ -35,6 +35,7 @@ if (!is_null($text)) {
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	$result = curl_exec($ch);
 	curl_close($ch);
-	echo $result . "\r\n";	
+	echo "Success" . "\r\n";
+	'echo $result . "\r\n";	
 }
 ?>
