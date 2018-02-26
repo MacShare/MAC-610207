0335df798c7f98eab45036aaa770f5b98ac8b05c
MacShare <cmod.maod@gmail.com>
2018-02-26 22:12:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -130,7 +130,7 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// License Check
 			include 'MacLicense.php';
-			$UserBypass = "True"
+			$UserBypass = "True";
 			//Select Group
 			if (($togroupid == 'Cd90b89c39f5a695f6d6996c80829e269') or ($togroupid == 'Cc7ac9ccc51f05b2a60a1abed8cf85723') or ($touserid == 'U554a18dbd36996fdb3dd95c218cf6db0') or ($userlicense == 'true') or ($UserBypass == 'True')) {
 				switch ($text) {
